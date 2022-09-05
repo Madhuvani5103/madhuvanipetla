debaa78f6971413cbc9f5f1c641e1c33156d7c78
Madhuvani5103 <77262484+Madhuvani5103@users.noreply.github.com>
2022-09-05 11:59:20 +0530

    First Commit

---
+++ assets/js/email.js
@@ -0,0 +1,14 @@
+function sendEmail(){
+    // Email.send({
+    //     Host : "smtp.gmail.com",
+    //     Username : "madhuvanipetla@gmail.com",
+    //     Password : "",
+    //     To : 'madhuvanip2003@gmail.com',
+    //     From : document.getElementById("email").value,
+    //     Subject : "Website Mail",
+    //     Body : "this is the body"
+    // }).then(
+    //   message => alert(message)
+    // );
+    // console.log(document.getElementById("email").value);
+}
